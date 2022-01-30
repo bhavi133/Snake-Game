954ac5d69ead7de9c6057e88e34a8b96d12100ce
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-01-30 11:20:11 +0530

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -65,3 +65,46 @@ scoring.penup()
 scoring.hideturtle()
 scoring.goto(0, 300)
 scoring.write("Your Score: ", align="center", font=("Courier", 24, "bold"))
+
+# define how to move
+
+def snake_go_up():
+    if snake.direction != "down":
+        snake.direction = "up"
+
+def snake_go_down():
+    if snake.direction != "up":
+        snake.direction = "down"
+
+def snake_go_left():
+    if snake.direction != "right":
+        snake.direction = "left"
+
+def snake_go_right():
+    if snake.direction != "left":
+        snake.direction = "right"
+
+def snake_move():
+    if snake.direction == "up":
+        y = snake.ycor()
+        snake.sety(y + 20)
+
+    if snake.direction == "down":
+        y = snake.ycor()
+        snake.sety(y - 20)
+
+    if snake.direction == "left":
+        x = snake.xcor()
+        snake.setx(x - 20)
+
+    if snake.direction == "right":
+        x = snake.xcor()
+        snake.setx(x + 20)
+
+# keyboard binding
+
+screen.listen()
+screen.onkeypress(snake_go_up, "Up")
+screen.onkeypress(snake_go_down, "Down")
+screen.onkeypress(snake_go_left, "Left")
+screen.onkeypress(snake_go_right, "Right")
