7982cff2e822da307df793113fc9b60d38c6e5ed
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-01-31 12:21:36 +0530

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -108,3 +108,60 @@ screen.onkeypress(snake_go_up, "Up")
 screen.onkeypress(snake_go_down, "Down")
 screen.onkeypress(snake_go_left, "Left")
 screen.onkeypress(snake_go_right, "Right")
+
+# main loop
+
+while True:
+    screen.update()
+
+    # snake and fruit collision
+    if snake.distance(fruit) < 20:
+        x = random.randint(-290, 270)
+        y = random.randint(-240, 240)
+        fruit.goto(x, y)
+        scoring.clear()
+        score += 1
+        scoring.write("Score : {}".format(score), align="center", font=("Courier", 24, "bold"))
+        delay -= 0.001
+        # creating new food
+        new_fruit = turtle.Turtle()
+        new_fruit.speed(0)
+        new_fruit.shape('square')
+        new_fruit.color('turquoise')
+        new_fruit.penup()
+        old_fruit.append(new_fruit)
+
+    # adding ball to snake
+    for index in range(len(old_fruit) - 1, 0, -1):
+        a = old_fruit[index - 1].xcor()
+        b = old_fruit[index - 1].ycor()
+
+        old_fruit[index].goto(a, b)
+
+    if len(old_fruit) > 0:
+        a = snake.xcor()
+        b = snake.ycor()
+        old_fruit[0].goto(a, b)
+    snake_move()
+
+    # snake and border collision
+    if snake.xcor() > 280 or snake.xcor() < -300 or snake.ycor() > 240 or snake.ycor() < -240:
+        time.sleep(1)
+        screen.clear()
+        screen.bgcolor('black')
+        scoring.goto(0, 0)
+        scoring.write("    Game Over! \n  Your score is {}".format(score), align="center", font=("Courier", 30, "bold"))
+
+    # snake collision
+    for food in old_fruit:
+        if food.distance(snake) < 20:
+            time.sleep(1)
+            screen.clear()
+            screen.bgcolor('black')
+            scoring.goto(0, 0)
+            scoring.write("    GAME OVER \n Your Score is {}".format(score), align="center",
+                          font=("Courier", 30, "bold"))
+
+    time.sleep(delay)
+
+turtle.Terminator()
